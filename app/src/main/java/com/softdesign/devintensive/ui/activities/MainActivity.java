7eb108b5ed6fc18da46f632a58f7af1786cc819b
rsdev-atk <rsdev.info@gmail.com>
2016-06-27 17:32:24 +0700

    Home work 2 complete

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,23 +1,48 @@
 package com.softdesign.devintensive.ui.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     public static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity ";
 
+    private ImageView progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+
+        progress = (ImageView)findViewById(R.id.progress);
+        progress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               showProgress();
+                runWithDelay();
+            }
+        });
+
     }
+
+    private void runWithDelay(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                hideProgress();
+            }
+        },5000);
+    }
+
+
 
 
     @Override
